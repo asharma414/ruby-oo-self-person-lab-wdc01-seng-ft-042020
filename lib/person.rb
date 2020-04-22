99c76683d8b8e2ce19c59c71665721f1c08bdf35
asharma414 <abhi.sharm.414@gmail.com>
2020-04-22 17:31:07 -0400

    refactoring code using += operator

--- lib/person.rb
+++ lib/person.rb
@@ -38,36 +38,36 @@ class Person
     end
 
     def get_paid(salary)
-        @bank_account += salary
+        self.bank_account += salary
         return "all about the benjamins"
     end
 
     def take_bath
-        self.hygiene = @hygiene + 4
+        self.hygiene += 4
         return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.hygiene = @hygiene - 3
-        self.happiness = @happiness + 2
+        self.hygiene -= 3
+        self.happiness += 2
         return "♪ another one bites the dust ♫"
     end
 
     def call_friend(person)
-        self.happiness = @happiness + 3
-        person.happiness = person.happiness + 3
+        self.happiness += 3
+        person.happiness += 3
         return "Hi #{person.name}! It's #{self.name}. How are you?"
     end
 
     def start_conversation(person, topic)
         case topic
         when 'politics'
-            self.happiness = @happiness - 2
-            person.happiness = person.happiness - 2
+            self.happiness -= 2
+            person.happiness -= 2
             return 'blah blah partisan blah lobbyist'
         when 'weather'
-            self.happiness = @happiness + 1
-            person.happiness = person.happiness + 1
+            self.happiness += 1
+            person.happiness += 1
             return 'blah blah sun blah rain'
         else 
             return 'blah blah blah blah blah'
