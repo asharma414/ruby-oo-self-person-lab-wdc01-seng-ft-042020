b21dee01cd1f625f6e7bd581bcad67b64bc089a8
asharma414 <abhi.sharm.414@gmail.com>
2020-04-22 14:41:22 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -46,11 +46,49 @@ class Person
     end
 
     def clean?
-        if self.hygiene > 7
+        if @hygiene > 7
             true
         else
             false
         end
     end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = @hygiene + 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene = @hygiene - 3
+        self.happiness = @happiness + 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person)
+        self.happiness = @happiness + 3
+        person.happiness = person.happiness + 3
+        return "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        case topic
+        when 'politics'
+            self.happiness = @happiness - 2
+            person.happiness = person.happiness - 2
+            return 'blah blah partisan blah lobbyist'
+        when 'weather'
+            self.happiness = @happiness + 1
+            person.happiness = person.happiness + 1
+            return 'blah blah sun blah rain'
+        else 
+            return 'blah blah blah blah blah'
+        end
+    end
+
 end
 
