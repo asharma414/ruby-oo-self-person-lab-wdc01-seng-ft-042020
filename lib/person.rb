a1ef79f928bb611c9f4f1ff4eea144ac1998f301
asharma414 <abhi.sharm.414@gmail.com>
2020-04-22 14:22:05 -0400

    first commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,56 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        if num > 10
+            @happiness = 10
+        elsif num < 0
+            @happiness = 0
+        else
+            @happiness = num
+        end
+    end
+
+    def happiness
+        @happiness
+    end
+
+    def hygiene=(num)
+        if num > 10
+            @hygiene = 10
+        elsif num < 0
+            @hygiene = 0
+        else
+            @hygiene = num
+        end
+    end
+
+    def hygiene
+        @hygiene
+    end
+
+    def happy?
+        if self.happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            true
+        else
+            false
+        end
+    end
+end
+
