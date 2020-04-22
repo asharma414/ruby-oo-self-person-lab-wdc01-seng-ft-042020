d3929e34a62fc251a7363387cb9fe3892c29964e
asharma414 <abhi.sharm.414@gmail.com>
2020-04-22 17:25:15 -0400

    refactoring code

--- lib/person.rb
+++ lib/person.rb
@@ -30,19 +30,11 @@ class Person
     end
 
     def happy?
-        if self.happiness > 7
-            true
-        else
-            false
-        end
+        self.happiness > 7
     end
 
     def clean?
-        if @hygiene > 7
-            true
-        else
-            false
-        end
+       self.hygiene > 7
     end
 
     def get_paid(salary)
